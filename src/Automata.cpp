fc78135d6af134dada418d57228b5b5a6b91f00f
Рамазан Чопсиев <chopsiev.ramazan@yandex.ru>
2023-05-31 21:04:35 +0300

    Версия 1.03

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -10,34 +10,26 @@ Automata::Automata() {
 
 void Automata::on() {
     state = WAIT;
-    std::cout << "Автомат включен. Готов к работе." << std::endl;
 }
 
 void Automata::off() {
     state = OFF;
-    std::cout << "Автомат выключен." << std::endl;
 }
 
 void Automata::coin(int money) {
     if (state == ACCEPT || state == WAIT) {
         cash += money;
         state = ACCEPT;
-        std::cout << "Внесено " << money << " рублей." << std::endl;
-    }
-    else {
-        std::cout << "Невозможно принять деньги." << std::endl;
+        std::cout << " " << money << " " << std::endl;
     }
 }
 
 void Automata::getMenu() {
     if (state == WAIT) {
-        std::cout << "Меню напитков:" << std::endl;
+        
         for (int i = 0; i < 3; i++) {
-            std::cout << i + 1 << "." << menu[i] << "-" << prices[i] << " р" << std::endl;
+            std::cout<<i+1<<"."<<menu[i]<<"-"<<prices[i]<<" "<<std::endl;
         }
-    }
-    else {
-        std::cout << "Меню доступно только в состоянии ожидания." << std::endl;
     }
 }
 
@@ -50,19 +42,10 @@ void Automata::choice(int option) {
         if (option >= 1 && option <= 3) {
             if (check()) {
                 state = COOK;
-                std::cout << "Приготовление напитка..." << std::endl;
+
                 cook();
             }
-            else {
-                std::cout << "Недостаточно средств." << std::endl;
-            }
         }
-        else {
-            std::cout << "Некорректный выбор напитка." << std::endl;
-        }
-    }
-    else {
-        std::cout << "Внесите денеги." << std::endl;
     }
 }
 
@@ -79,20 +62,16 @@ void Automata::cancel() {
     if (state == ACCEPT) {
         cash = 0;
         state = WAIT;
-        std::cout << "Сеанс обслуживания отменен." << std::endl;
-    }
-    else {
-        std::cout << "Нельзя отменить сеанс обслуживания." << std::endl;
     }
 }
 
 void Automata::cook() {
-    std::cout << "Напиток готов!" << std::endl;
+    
     finish();
 }
 
 void Automata::finish() {
     cash = 0;
     state = WAIT;
-    std::cout << "Спасибо! Приходите еще!" << std::endl;
+    
 }
